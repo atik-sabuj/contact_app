0150c75095d0fa8c7953b6ebc132890390936647
Atikuzzaman Sabuj <sabuj235@gmail.com>
2022-10-13 20:09:23 +0600

    Initial commit

---
+++ lib/contact_provider.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_ml_vision/google_ml_vision.dart';
+
+import 'models/contact_model.dart';
+
+class ContactProvider extends ChangeNotifier {
+  List<String?> textLines = [];
+  List<ContactModel> contactList = [
+    ContactModel(id: 1, name: 'ABC', mobile: '01711000001', email: 'abc@gmail.com', streetAddress: 'Kawranbazar, Dhaka', website: 'www.google.com', image: 'images/person.png'),
+    ContactModel(id: 2, name: 'DEF', mobile: '01711000002', email: 'def@gmail.com', streetAddress: 'Dhanmondi, Dhaka', website: 'flutter.dev', favorite: true ,image: 'images/person.png'),
+    ContactModel(id: 3, name: 'GHI', mobile: '01711000003', email: 'ghi@gmail.com', streetAddress: 'Mirpur, Dhaka', website: 'flutter.dev', image: 'images/person.png'),
+  ];
+
+  void updateFavorite(int id) {
+    final contact = contactList.firstWhere((element) => element.id == id);
+    final index = contactList.indexOf(contact);
+    contactList[index].favorite = !contactList[index].favorite;
+    notifyListeners();
+  }
+
+  Future<void> processCardImage(String imagePath) async {
+    textLines.clear();
+    final GoogleVisionImage visionImage = GoogleVisionImage.fromFile(File(imagePath));
+    final TextRecognizer textRecognizer = GoogleVision.instance.textRecognizer();
+    final VisionText visionText = await textRecognizer.processImage(visionImage);
+    for(var block in visionText.blocks) {
+      for(var line in block.lines) {
+        textLines.add(line.text);
+      }
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:contact_app/contact_provider.dart';
+import 'package:contact_app/pages/homepage.dart';
+import 'package:contact_app/pages/scan_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:provider/provider.dart';
+
+import 'pages/contact_details.dart';
+import 'pages/image_processing_page.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => ContactProvider(),
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+     initialRoute: HomePage.routeName,
+     builder: EasyLoading.init(),
+     routes: {
+        HomePage.routeName : (context) => HomePage(),
+        ContactDetails.routeName : (context) => ContactDetails(),
+        ScanPage.routeName : (context) => ScanPage(),
+        ImageProcessingPage.routeName : (context) => ImageProcessingPage(),
+     },
+    );
+  }
+}
+
+
